bee5e98ce336d4c20a29c0bfb7dc321cd15f617c
Wenstrand <douglas.wenstrand@jhuapl.edu>
2024-02-20 22:31:53 -0500

    updated to 2023.2

--- make_project.bat
+++ make_project.bat
@@ -1,4 +1,4 @@
-call c:\Xilinx\Vivado\2022.1\settings64.bat
+call c:\Xilinx\Vivado\2023.2\settings64.bat
 call vivado -nolog -nojournal -mode=batch -source=make_project.tcl
 call vivado -nolog -nojournal -mode=batch -source=impl.tcl
 call xsct make_sw.tcl
